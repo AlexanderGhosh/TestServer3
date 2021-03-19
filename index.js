f0b0e382f249a86cc3d425ee542942f24b94d2d9
Alexander Ghosh <ghoshalexander@gmail.com>
2021-03-19 15:45:16 +0000

    should now get ip adress

--- index.js
+++ index.js
@@ -14,5 +14,12 @@ app.get('/data', (req, res) => {
 });
 
 app.listen(port, () =>{
-  console.log('Listening on: http://localhost:%s', port);
+  var ipAddr = req.headers["x-forwarded-for"];
+  if (ipAddr){
+    var list = ipAddr.split(",");
+    ipAddr = list[list.length-1];
+  } else {
+    ipAddr = req.connection.remoteAddress;
+  }
+  console.log('Listening on: http://%s:%s', ipAddr, port);
 });
