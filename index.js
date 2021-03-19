a541c5388c5879649e10a8da0cdaa95247bd1f4e
Alexander Ghosh <ghoshalexander@gmail.com>
2021-03-19 15:48:54 +0000

    moved the get ip func

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const app = express();
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+  var ipAddr = req.headers["x-forwarded-for"];
+  if (ipAddr){
+    var list = ipAddr.split(",");
+    ipAddr = list[list.length-1];
+  } else {
+    ipAddr = req.connection.remoteAddress;
+  }
+  console.log(ipAddr);
   res.send("First Page");
 });
 
@@ -14,12 +22,5 @@ app.get('/data', (req, res) => {
 });
 
 app.listen(port, () =>{
-  var ipAddr = req.headers["x-forwarded-for"];
-  if (ipAddr){
-    var list = ipAddr.split(",");
-    ipAddr = list[list.length-1];
-  } else {
-    ipAddr = req.connection.remoteAddress;
-  }
-  console.log('Listening on: http://%s:%s', ipAddr, port);
+  console.log('Listening on: http://localhost:%s', port);
 });
