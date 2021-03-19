2ddf5cdb1b00a158bd25c245b9c8d540d3090757
Alexander Ghosh <ghoshalexander@gmail.com>
2021-03-19 12:43:33 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const data = require('./data.json');
+
+const app = express();
+
+let port = process.env || 3000;
+
+app.get("/", (req, res) =?{
+  res.send("First Page");
+});
+
+app.get('/data', (req, res) =>{
+  res.send(data);
+});
+
+app.listen(port, () =>{
+  console.log('Listening on: http://localhost:${port}');
+});
